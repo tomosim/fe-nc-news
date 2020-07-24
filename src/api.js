78a342b0afcf2950ae9b445c5841f38d427cf63f
Tom Simmons <tom_simmons@live.co.uk>
2020-07-24 12:48:27 +0100

    create SingleArticle component

--- src/api.js
+++ src/api.js
@@ -5,3 +5,9 @@ export const fetchArticles = () => {
     .get("https://toms-nc-news-be.herokuapp.com/api/articles")
     .then(({ data }) => data.articles);
 };
+
+export const fetchArticleById = (article_id) => {
+  return axios
+    .get(`https://toms-nc-news-be.herokuapp.com/api/articles/${article_id}`)
+    .then(({ data }) => data.article);
+};
--- src/pages/SingleArticle.js
+++ src/pages/SingleArticle.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { fetchArticleById } from "../api";
 
 const SingleArticle = ({ article_id }) => {
+  const [article, setArticle] = useState({});
+
+  useEffect(() => {
+    fetchArticleById(article_id).then((article) => {
+      setArticle(article);
+    });
+  });
+
   return (
     <div>
-      <h1>Article</h1>
+      <h1>{article.title}</h1>
+      <p>{article.body}</p>
+      <h2>Comments</h2>
+      blah blah blah
     </div>
   );
 };
