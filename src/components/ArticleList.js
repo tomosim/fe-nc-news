41b2400c75d304460e01fe738aea2bacdbd04057
Tom Simmons <tom_simmons@live.co.uk>
2020-07-29 13:03:48 +0100

    add loading state to article list

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -5,14 +5,18 @@ import ArticleCard from "./ArticleCard";
 
 const ArticleList = () => {
   const [articles, setArticles] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     fetchArticles().then((articles) => {
       setArticles(articles);
+      setIsLoading(false);
     });
   });
 
-  return (
+  return isLoading ? (
+    <p>Loading</p>
+  ) : (
     <ul>
       {articles.map((article) => {
         return <ArticleCard article={article} key={article.article_id} />;
