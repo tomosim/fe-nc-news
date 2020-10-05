c553fb210c5de707af5dd5f597e389a5614ffdda
Tom <TomSimmons@Toms-MBP-3.fritz.box>
2020-10-05 16:40:46 +0200

    navbar styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "@reach/router";
 import { fetchTopics } from "../api";
+import styles from "../styles/navbar.module.css";
 
 const Navbar = ({ loggedInUser, setLoggedInUser }) => {
   const [topics, setTopics] = useState([]);
@@ -13,26 +14,52 @@ const Navbar = ({ loggedInUser, setLoggedInUser }) => {
   });
 
   return (
-    <nav>
-      <Link to="/">Home</Link>
-      <button onClick={() => setSubNavIsOpen(!subNavIsOpen)}>Topics</button>
-      {loggedInUser && <Link to="/new-article">Post New Article</Link>}
-      {loggedInUser === "" ? (
-        <Link to="/log-in">Log In</Link>
-      ) : (
-        <>
-          <Link to={`/user/${loggedInUser}`}>My Profile</Link>
-          <button onClick={() => setLoggedInUser("")}>Log Out</button>
-        </>
-      )}
+    <nav class={styles.navbar}>
+      <div class={styles.toprow}>
+        <Link to="/" class={styles.link}>
+          Home
+        </Link>
+        <button
+          onClick={() => setSubNavIsOpen(!subNavIsOpen)}
+          class={styles.link}
+        >
+          Topics
+        </button>
+        {loggedInUser && (
+          <Link to="/new-article" class={styles.link}>
+            Post New Article
+          </Link>
+        )}
+        {loggedInUser === "" ? (
+          <Link to="/log-in" className={`${styles.link} ${styles.end}`}>
+            Log In
+          </Link>
+        ) : (
+          <>
+            <Link
+              to={`/user/${loggedInUser}`}
+              class={`${styles.link} ${styles.end}`}
+            >
+              My Profile
+            </Link>
+            <button onClick={() => setLoggedInUser("")} class={styles.link}>
+              Log Out
+            </button>
+          </>
+        )}
+      </div>
       {subNavIsOpen && (
-        <nav>
+        <div className={styles.bottomrow}>
           {topics.map((topic) => (
-            <Link to={`/${topic.slug}/articles`} key={topic.slug}>
+            <Link
+              to={`/${topic.slug}/articles`}
+              key={topic.slug}
+              class={styles.bottomlink}
+            >
               {topic.slug}
             </Link>
           ))}
-        </nav>
+        </div>
       )}
     </nav>
   );
