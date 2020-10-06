a66fe34b58161b827a98ccae0857abba86190ad3
Tom <TomSimmons@Toms-MBP-3.fritz.box>
2020-10-06 11:30:06 +0200

    article styling

--- src/pages/SingleArticle.js
+++ src/pages/SingleArticle.js
@@ -3,6 +3,8 @@ import { fetchArticleById, deleteArticle } from "../api";
 import { navigate, Link } from "@reach/router";
 import CommentList from "../components/CommentList";
 import Voter from "../components/Voter";
+import styles from "../styles/single-article.module.css";
+import formatTime from "../utils/format-time";
 
 const SingleArticle = ({ article_id, loggedInUser }) => {
   const [article, setArticle] = useState({});
@@ -22,21 +24,35 @@ const SingleArticle = ({ article_id, loggedInUser }) => {
   };
 
   return isLoading ? (
-    <p>Loading article</p>
+    <p className={styles.loading}>Loading article...</p>
   ) : (
-    <div>
-      <h1 className="header">{article.title}</h1>
-      <h2>
-        <Link to={`/user/${article.author}`}>{article.author}</Link>
-      </h2>
-      <p>{article.body}</p>
-      <Voter votes={article.votes} article_id={article_id} />
-      {loggedInUser === article.author && (
-        <button onClick={handleClick}>Delete Article</button>
-      )}
+    <>
+      <div className={styles.header}>
+        <h1>{article.title}</h1>
+        <div className={styles.info}>
+          <h2>
+            <Link to={`/user/${article.author}`} className={styles.author}>
+              {article.author}
+            </Link>
+          </h2>
+          <h2>{formatTime(new Date(article.created_at))}</h2>
+        </div>
+      </div>
+
+      <div className={styles.articleContent}>
+        <Link to={`/${article.topic}/articles`} className={styles.topic}>
+          {article.topic}
+        </Link>
+        <p className={styles.body}>{article.body}</p>
+        <Voter votes={article.votes} article_id={article_id} />
+        {loggedInUser === article.author && (
+          <button onClick={handleClick}>Delete Article</button>
+        )}
+      </div>
+
       <h2>Comments</h2>
       <CommentList article_id={article_id} loggedInUser={loggedInUser} />
-    </div>
+    </>
   );
 };
 
